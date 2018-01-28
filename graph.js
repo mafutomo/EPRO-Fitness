84ec67d15ffc5cf6f339f8a7fdf8b2ba849f5168
Stephanie Marvez <marvez@gmail.com>
2018-01-28 14:49:15 -0700

    both progesterone and estrogen on chart as bars

--- graph.js
+++ graph.js
@@ -1,127 +1,88 @@
+var margin = {top: 80, right: 80, bottom: 80, left: 80},
+    width = 800 - margin.left - margin.right,
+    height = 400 - margin.top - margin.bottom;
 
+var x = d3.scaleBand()
+    .range([0, width], .1);
 
-var data = d3.csv("non_hormonal_bc.csv", function(err, data){
-  if (err) {
-    return console.log(err);
-  }
-  generate(data);
-  console.log(data);
-  console.log(data.length);
-})
+var y0 = d3.scaleLinear()
+  .domain([300, 1100])
+  .range([height, 0]),
+y1 = d3.scaleLinear()
+  .domain([20, 80])
+  .range([height, 0]);
 
-var n = 2, // The number of series.
-    m = 28; // The number of values per series.
+var xAxis = d3.axisBottom()
+    .scale(x)
 
-function generate(data){
-  var chart_width     =   800;
-  var chart_height    =   400;
-  var padding         =   50;
-  var barPadding = 2;
+// create left yAxis
+var yAxisLeft = d3.axisLeft()
+  .scale(y0)
+  .ticks(4)
 
-  // Create SVG Element
-  var svg  =  d3.select( '#chart' )
-      .append( 'svg' )
-      .attr( 'width', chart_width )
-      .attr( 'height', chart_height );
+// create right yAxis
+var yAxisRight = d3.axisRight()
+  .scale(y1)
+  .ticks(6)
 
-  // Create Scales
-  var x_scale = d3.scaleBand()
-    .domain(d3.range(data.length))
-    .rangeRound([padding, chart_width-padding])
-    .paddingInner(0.05)
+//create chart
+var svg = d3.select("#chart").append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+    .attr("class", "graph")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-  // var x_scale = d3.scaleLinear()
-  //     .domain([0, 28])
-  //     .range([ padding, chart_width - padding ])
-
-  var y_scaleLeft = d3.scaleLinear()
-      .domain([ 0, 400])
-      .range([ chart_height - padding, padding ]);
-
-  var y_scaleRight = d3.scaleLinear()
-      .domain([0, 10])
-      .range([chart_height - padding, padding])
-
-  // var r_scale = d3.scaleLinear()
-  //     .domain([0, d3.max(data, function( d ){
-  //         return d[1];
-  //     })])
-  //     .range([5, 30]);
-  //
-  // var a_scale = d3.scaleSqrt()
-  //     .domain([ 0, d3.max(data, function(d) {
-  //         return d[1];
-  //     })])
-  //     .range([ 0, 25 ]);
-
-  //create axis
-  var xAxis = d3.axisBottom()
-    .scale(x_scale)
-    .ticks(28)
-
-  var yAxisLeft = d3.axisLeft()
-    .scale(y_scaleLeft)
-    .ticks(5)
-    // .tickFormat(function(d){
-    //   return d + '%'
-    // })
-
-  var yAxisRight = d3.axisRight()
-    .scale(y_scaleRight)
-    .ticks(5)
-
-  svg.append('g')
-    .attr("class", "x-axis")
-    .attr("transform", "translate(0," + (chart_height-padding) + ")")
-    .call(xAxis)
-
-  svg.append('g')
-    .attr("class", "y-axis")
-    .attr("transform", "translate(" + padding + ",0)")
-    .call(yAxisLeft)
-
-  svg.append('g')
-    .attr("class", "y-axis")
-    .attr("transform", "translate(" + (chart_width - padding) + ",0)")
-    .call(yAxisRight)
-
-    // Bind Data and create bars
-
-  svg.selectAll("rect")
-    .data(data)
-    .enter()
-    .append("rect")
-    .attr( 'x', function( d, i ){
-        return x_scale(i);
-    })
-  //   .attr('x', function(d, i){
-  //   return i * ((chart_width - padding*2)/data.length) + padding;
-  // })
-  .attr('y', function(d){
-    return chart_height - d.estrogen - padding;
+ var data = d3.csv("non_hormonal_bc.csv", function(err, data){
+   data.forEach(function(d) {
+    d.day = +d.day;
+    d.estrogen = +d.estrogen;
+    d.progesterone = +d.progesterone;
   })
-  // .attr("width", 5)
-  .attr( 'width', x_scale.bandwidth() )
-  // .attr("width", (chart_width - padding )/data.length - barPadding)
-  .attr("height", function(d){
-    return d.estrogen;
-  })
-  .attr("fill", "#7ED26D")
-}
+   x.domain(data.map(function(d) { return d.day; }));
+   y0.domain([0, d3.max(data, function(d) { return d.estrogen; })]);
+   y1.domain([0, d3.max(data, function(d) { return d.progesterone; })]);
 
+  svg.append("g")
+      .attr("class", "x axis")
+      .attr("transform", "translate(0," + height + ")")
+      .call(xAxis);
+  svg.append("g")
+	  .attr("class", "y axis axisLeft")
+	  .attr("transform", "translate(0,0)")
+	  .call(yAxisLeft)
+	  .append("text")
+	  .attr("y", 6)
+	  .attr("dy", "-2em")
+	  .style("text-anchor", "end")
+	  .style("text-anchor", "end")
+	  .text("Estrogen(pg/ml)");
 
-// Create Labels
-// svg.append("g")
-//     .selectAll( 'text' )
-//     .data( data )
-//     .enter()
-//     .append( 'text' )
-//     .text(function(d) {
-//         return d.join( ',' );
-//     })
-//     .attr("x", function(d) {
-//         return x_scale(d[0]);
-//     })
-//     .attr("y", function(d) {
-//         return y_scale(d[1]);
-//     });
+  svg.append("g")
+	  .attr("class", "y axis axisRight")
+	  .attr("transform", "translate(" + (width) + ",0)")
+	  .call(yAxisRight)
+	  .append("text")
+	  .attr("y", 6)
+	  .attr("dy", "-2em")
+	  .attr("dx", "2em")
+	  .style("text-anchor", "end")
+	  .text("Progesterone(ng/ml)");
+
+  var bars = svg.selectAll(".bar").data(data).enter();
+  bars.append("rect")
+      .attr("class", "bar1")
+      .attr("x", function(d) { return x(d.day); })
+      .attr("width", x.bandwidth()/2)
+      .attr("y", function(d) { return y0(d.estrogen); })
+	    .attr("height", function(d,i,j) { return height - y0(d.estrogen); })
+      .attr("fill", "#6BBAA7");
+  bars.append("rect")
+      .attr("class", "bar2")
+      .attr("x", function(d) { return x(d.day) + x.bandwidth()/2; })
+      .attr("width", x.bandwidth() / 2)
+      .attr("y", function(d) { return y1(d.progesterone); })
+      .attr("height", function(d,i,j) { return height - y1(d.progesterone);
+   })
+      .attr("fill", "#FBA100");;
+ })
