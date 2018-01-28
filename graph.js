4acd4bccad4be70cefbb04aa3250b947cc079ff1
Stephanie Marvez <marvez@gmail.com>
2018-01-27 22:39:32 -0700

    bars on a chart

--- graph.js
+++ graph.js
@@ -4,14 +4,16 @@ var data = d3.csv("non_hormonal_bc.csv", function(err, data){
   if (err) {
     return console.log(err);
   }
-  console.log(data[0].estrogen);
+  console.log(data[5].day);
   generate(data);
 })
 
-function generate(dataset){
+function generate(data){
   var chart_width     =   800;
   var chart_height    =   400;
   var padding         =   50;
+  var barPadding = 2;
+  console.log(data[2].day);
 
   // Create SVG Element
   var svg  =  d3.select( '#chart' )
@@ -21,10 +23,8 @@ function generate(dataset){
 
   // Create Scales
   var x_scale = d3.scaleLinear()
-      .domain([0, d3.max(data, function(d){
-          return d[0];
-      })])
-      .range([ padding, chart_width - padding * 2 ]);
+      .domain([0, 28])
+      .range([ padding, chart_width - padding ]);
 
   var y_scaleLeft = d3.scaleLinear()
       .domain([ 0, 400])
@@ -34,17 +34,17 @@ function generate(dataset){
       .domain([0, 10])
       .range([chart_height - padding, padding])
 
-  var r_scale = d3.scaleLinear()
-      .domain([0, d3.max(data, function( d ){
-          return d[1];
-      })])
-      .range([5, 30]);
-
-  var a_scale = d3.scaleSqrt()
-      .domain([ 0, d3.max(data, function(d) {
-          return d[1];
-      })])
-      .range([ 0, 25 ]);
+  // var r_scale = d3.scaleLinear()
+  //     .domain([0, d3.max(data, function( d ){
+  //         return d[1];
+  //     })])
+  //     .range([5, 30]);
+  //
+  // var a_scale = d3.scaleSqrt()
+  //     .domain([ 0, d3.max(data, function(d) {
+  //         return d[1];
+  //     })])
+  //     .range([ 0, 25 ]);
 
   //create axis
   var xAxis = d3.axisBottom()
@@ -75,6 +75,23 @@ function generate(dataset){
     .attr("class", "y-axis")
     .attr("transform", "translate(" + (chart_width - padding) + ",0)")
     .call(yAxisRight)
+
+    // Bind Data and create bars
+
+  svg.selectAll("rect")
+    .data(data)
+    .enter()
+    .append("rect")
+    .attr('x', function(d, i){
+    return i * (chart_width/data.length) + padding;
+  })
+  .attr('y', function(d){
+    return chart_height - d.estrogen - padding;
+  })
+  .attr("width", chart_width/data.length - barPadding)
+  .attr("height", function(d){
+    return d.estrogen;
+  })
 }
 
 
