5c51a3087330da404aa2fe74511a57b1488bb33c
nigelflippo <flippo.nigel@gmail.com>
2018-01-29 22:57:41 -0700

    login logout

--- src/components/login.js
+++ src/components/login.js
@@ -10,10 +10,45 @@ class Login extends Component {
 
   constructor(props) {
     super(props);
+    this.state = {
+      email: '',
+      password: '',
+      token: '',
+      message: ''
+    }
   }
 
-  loginUser = async (email, password) => {
-    const response = await fetch ()
+  loginUser = async (e, {email, password}) => {
+    e.preventDefault()
+    const response = await fetch ('https://epro-api.herokuapp.com/auth/login', {
+      method: 'POST',
+      body: JSON.stringify({
+        email,
+        password
+      }),
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      }
+    })
+    const logged = await response.json()
+    if (logged.auth_token) {
+      this.setState({
+        token: logged.auth_token,
+        message: logged.message
+      })
+      localStorage.setItem('token', logged.auth_token)
+    } else {
+      this.setState({
+        message: logged.message
+      })
+    }
+  }
+
+  handleChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value
+    })
   }
 
   render() {
@@ -21,24 +56,30 @@ class Login extends Component {
       <div>
         <p className="title-app">E/Pro</p>
         <br />
-
+        <form onSubmit={(e)=>{this.loginUser(e, this.state)}}>
         <TextField
         hintText="example@email.com"
         floatingLabelText="Email Login"
+        value={this.state.email}
+        onChange={this.handleChange}
+        name="email"
         /><br />
 
         <TextField
           hintText=""
           floatingLabelText="Password"
           type="password"
+          value={this.state.password}
+          onChange={this.handleChange}
+          name="password"
         /><br />
          <br />
           <br />
 
+
+         <RaisedButton label="Login" backgroundColor='#52BFAB' labelColor='white' style={style} type="submit"/>
+       </form>
          <RaisedButton label="Register" backgroundColor='#52BFAB' labelColor='white'  style={style}/>
-
-         <RaisedButton label="Login" backgroundColor='#52BFAB' labelColor='white' style={style}/>
-
       </div>
     )
   }
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -16,8 +16,27 @@ class Navbar extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      open: false
+      open: false,
+      token: '',
+      message: ''
     }
+  }
+
+  logout = async () => {
+    const r = await fetch(`https://epro-api.herokuapp.com/auth/logout`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+        'Authorization': localStorage.getItem('token')
+      }
+    })
+    const logged = await r.json()
+    this.setState({
+      token: logged.auth_token,
+      message: logged.message
+    })
+    localStorage.removeItem('token')
   }
 
   handleToggle = () => this.setState({open: !this.state.open});
@@ -55,7 +74,7 @@ class Navbar extends Component {
                 </Link>
 
                 <Link to={`/login`}>
-                <MenuItem style={{fontFamily: 'Julius Sans One'}}>Logout</MenuItem>
+                <MenuItem style={{fontFamily: 'Julius Sans One'}} onClick={()=>{this.logout()}}>Logout</MenuItem>
                 </Link>
 
               </Drawer>
